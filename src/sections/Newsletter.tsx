3cffd761bd7010823d89e818e3a1c8dfe2e2b907
zarakikenpashi <sadiarog.abel@gmail.com>
2025-04-08 19:25:22 +0000

    customize navbar

--- src/sections/Newsletter.tsx
+++ src/sections/Newsletter.tsx
@@ -5,9 +5,9 @@ const Newsletter = () => {
     <div className="px-5 pt-12">
       <h1 className="text-xl py-5 font-bold">Souscrire</h1>
       <div className="flex items-center rounded-lg border-gray-200 border py-1 px-2 space-x-2">
-        <Mail className="text-gray-400 size-4" />
+        <Mail className="text-gray-400 size-4 hidden md:block" />
         <input type="text" className="outline-none w-full" placeholder="Entrer votre adresse email" />
-        <button className="flex items-center py-2 px-2.5 bg-gray-800 cursor-pointer text-white rounded-md">
+        <button className="flex items-center md:py-2 md:px-2.5 bg-gray-800 cursor-pointer text-white rounded-md sm:text-sm px-1 py-1">
             <span>Rejoindre</span>
             <ArrowRight />
         </button>
