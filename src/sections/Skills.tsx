871641175c9b469de2fd5c652ab11ae2ea12d658
zarakikenpashi <sadiarog.abel@gmail.com>
2025-04-08 13:05:35 +0000

    customization de la section skills

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -8,22 +8,8 @@ import React from "../components/icons/React"
 const Skills = () => {
   return (
     <div className="px-5 pt-12">
-      <h1 className="text-xl py-5">Skills</h1>
+      <h1 className="text-xl py-5">Compétences</h1>
       <div className="flex flex-col space-y-4">
-        <div className="flex items-center space-x-8 space-y-2 md:space-y-0  flex-wrap">
-          <h1>Design Tools:</h1> 
-          <div className="flex space-x-4 items-center">
-            <div className="flex items-center">
-              <Figmas />
-              Figma, 
-            </div>
-            <div className="flex items-center">
-              <Sketchs />
-              Sketch 
-            </div>
-          </div>
-        </div>
-
         <div className="flex items-center space-x-8 space-y-2 md:space-y-0 flex-wrap">
           <h1>Development:</h1> 
           <div className="flex space-x-4 items-center flex-wrap">
